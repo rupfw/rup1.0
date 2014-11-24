2aaf14b598b157994bc29d5b02c715852b350f8c
waseem <mwaseemkha@gmail.com>
2014-11-24 22:22:20 +0500

    Badnet) working...

--- RUP1.0/Client/MyClient.py
+++ RUP1.0/Client/MyClient.py
@@ -7,40 +7,39 @@ blksz = 1024
 
 if len(sys.argv) < 2:
 	print 'Server Port No. missing in command line arguments'
-	serverControlPort = raw_input('Enter Server Port No.: ')
+	serverPort = raw_input('Enter Server Port No.: ')
 else:
-	serverControlPort = sys.argv[1]
+	serverPort = sys.argv[1]
 
 serverIP = '127.0.0.1'
-clientControlSocket = socket(AF_INET, SOCK_STREAM) 
+clientSocket = socket(AF_INET, SOCK_STREAM) 
 
 #Function Start here--
 def ReceiveFile(filename):
 	try:
-		filepath = "Client/"+filename
-		file = open(filepath, 'wb') # create local file in cwd
+		filepath = filename
+		file = open('testfile', 'wb') # create local file in cwd
 		while True:
-			data = clientControlSocket.recv(blksz) # get up to 1K at a time
+			data = clientSocket.recv(blksz) # get up to 1K at a time
 			if not data: break # till closed on server side
 			file.write(data) # store data in local file
-		print 'File Received.'
 		file.close()
+		print 'File Received.'
 		return 0
 	except:
+		print 'File Receiving Error'
 		file.close()
 		return -1
 #Function end here--
 
 try:
-	clientControlSocket.connect((serverIP,int (serverControlPort)))
-	file = "\Server\PLT.pdf"; 
-	clientControlSocket.send(file)
+	clientSocket.connect((serverIP,int (serverPort)))
+	file = "PLT.pdf"; 
+	clientSocket.send(file)
 	ReceiveFile(file)
-	ControlConRecv = clientControlSocket.recv(1024) 
-
+	ControlConRecv = clientSocket.recv(1024) 
 	print (ControlConRecv)
-
-	clientControlSocket.close()
+	clientSocket.close()
 except:
-	clientControlSocket.close()
+	clientSocket.close()
 
--- RUP1.0/Server/MYServer.py
+++ RUP1.0/Server/MYServer.py
@@ -5,10 +5,15 @@ import os.path
 import json
 import sys
 
+from BadNet0 import *
+#from BadNet1 import *
+#from BadNet2 import *
+#from BadNet3 import *
+#from BadNet4 import *
+#from BadNet5 import *
+
 serverIP = '127.0.0.1'
 blksz = 1024 #Bulk size for receiving file in chunks
-filePathDict = {} #Dictionary for path, size pairs
-
 
 if len(sys.argv) < 2:
 	print 'Server Port No. missing in command line arguments'
@@ -25,25 +30,24 @@ serverSocket.listen(10)
 
 print 'FTP Server Started successfully.\n' 
 
-def SendFile (filename,ClientIP, DataConPort):
+def SendFile (filename):
 	try:
-		file = open(filename, 'rb')
+		file = open('/home/waseem/Dropbox/rup1.0/RUP1.0/Server/PLT.pdf', 'rb')
 		while True:
 			bytes = file.read(blksz) # read/send 1K at a time
 			if not bytes: break # until file totally sent
-			sent = connectionSocket.send(bytes)
-			assert sent == len(bytes)
+			BadNet.transmit(connectionSocket,bytes,serverIP,serverPort)
 	except:
 		print('Error downloading file on server:', filename)
 
 while 1:
 	print 'The server is Waiting for a connection.'
-	filePathDict.clear()
 	try:
 		connectionSocket, addr = serverSocket.accept() 
 		clientIP = addr[0] 
 		filename = connectionSocket.recv(1024) 
-		
+		print (filename)
+		SendFile (filename)
 		connectionSocket.send("500") #500: Syntax error; unrecognized command
 		connectionSocket.close()
 		
